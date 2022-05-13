d3a0a37a5dedb56f1804f9ac2eaca263759328eb
Benedikt Karl <karl.bene.99@googlemail.com>
2022-05-13 18:18:15 +0200

    Upgrade gradle, minestom and hephaistos

--- StomStruct-Core/src/main/java/mom/minecraft/stomstruct/core/io/VanillaStructureWriter.java
+++ StomStruct-Core/src/main/java/mom/minecraft/stomstruct/core/io/VanillaStructureWriter.java
@@ -6,89 +6,104 @@ import org.jglrxavpok.hephaistos.nbt.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Writes NBT structure files compatible with vanilla structure blocks.
  */
-public class VanillaStructureWriter implements IStructureWriter{
+public class VanillaStructureWriter implements IStructureWriter {
 
-    @Override
-    public void write(Structure structure, String path) throws IOException {
+	@Override
+	public void write(Structure structure, String path) throws IOException {
 
-        NBTList<NBTCompound> structureBlocks = new NBTList<>(NBTTypes.TAG_Compound);
-        NBTList<NBTCompound> structureEntities = new NBTList<>(NBTTypes.TAG_Compound);
-        NBTList<NBTList<NBTCompound>> structurePalettes = new NBTList<>(NBTTypes.TAG_List);
-        NBTList<NBTInt> structureSize = new NBTList<>(NBTTypes.TAG_Int);
-        NBTInt structureDataVersion = new NBTInt(-1);
+		List<NBTCompound> structureBlocks = new ArrayList<>();
+		List<NBTCompound> structureEntities = new ArrayList<>();
+		List<List<NBTCompound>> structurePalettes = new ArrayList<>();
+		List<NBTInt> structureSize = new ArrayList<>();
+		NBTInt structureDataVersion = new NBTInt(-1);
 
-        // Populate blocks list
-        for (int i = 0; i < structure.getBlockCount(); i++)
-        {
-            // Add block state
-            NBTInt blockState = new NBTInt(structure.getBlockStates()[i]);
+		// Populate blocks list
+		for (int i = 0; i < structure.getBlockCount(); i++) {
+			// Add block state
+			NBTInt blockState = new NBTInt(structure.getBlockStates()[i]);
 
-            // Add block positions
-            NBTList<NBTInt> blockPos = new NBTList<>(NBTTypes.TAG_Int);
-            blockPos.add(new NBTInt(structure.getBlockPositions()[i][0]));
-            blockPos.add(new NBTInt(structure.getBlockPositions()[i][1]));
-            blockPos.add(new NBTInt(structure.getBlockPositions()[i][2]));
+			// Add block positions
+			NBTList<NBTInt> blockPos = NBT.List(NBTType.TAG_Int, List.of(
+					new NBTInt(structure.getBlockPositions()[i][0]),
+					new NBTInt(structure.getBlockPositions()[i][1]),
+					new NBTInt(structure.getBlockPositions()[i][2])
+			));
 
-            NBTCompound block = new NBTCompound();
-            block.set("pos", blockPos);
-            block.set("state", blockState);
+			final int index = i;
 
-            // Optionally add block NBT
-            if (structure.getBlockNBT()[i] != null) {
-                NBTCompound nbt = structure.getBlockNBT()[i];
-                block.set("nbt", nbt);
-            }
+			NBTCompound block = NBT.Compound(t -> {
+				t.set("pos", blockPos);
+				t.set("state", blockState);
 
-            structureBlocks.add(block);
-        }
+				// Optionally add block NBT
+				if (structure.getBlockNBT()[index] != null) {
+					NBTCompound nbt = structure.getBlockNBT()[index];
+					t.set("nbt", nbt);
+				}
+			});
 
-        // Populate palette(s)
-        for (int i = 0; i < structure.getPaletteCount(); i++) {
-            NBTList<NBTCompound> palette = new NBTList<>(NBTTypes.TAG_Compound);
+			structureBlocks.add(block);
+		}
 
-            for (int j = 0; j < structure.getBlockStatePalettes()[i].length; j++) {
-                NBTCompound swatch = new NBTCompound();
+		// Populate palette(s)
+		for (int i = 0; i < structure.getPaletteCount(); i++) {
+			List<NBTCompound> palette = new ArrayList<>();
 
-                Block block = structure.getBlockStatePalettes()[0][j];
-                swatch.set("Name", new NBTString(block.name()));
+			for (int j = 0; j < structure.getBlockStatePalettes()[i].length; j++) {
+				final int j_ = j;
+				NBTCompound swatch = NBT.Compound(t -> {
+					Block block = structure.getBlockStatePalettes()[0][j_];
+					t.set("Name", new NBTString(block.name()));
 
-                if (!block.properties().isEmpty()) {
-                    NBTCompound swatchProperties = new NBTCompound();
-                    for (String key: block.properties().keySet()) {
-                        swatchProperties.set(key, new NBTString(block.getProperty(key)));
-                    }
+					if (!block.properties().isEmpty()) {
+						NBTCompound swatchProperties = NBT.Compound(tp -> {
+							for (String key : block.properties().keySet()) {
+								tp.set(key, new NBTString(block.getProperty(key)));
+							}
+						});
 
-                    swatch.set("Properties", swatchProperties);
-                }
+						t.set("Properties", swatchProperties);
+					}
+				});
 
-                palette.add(swatch);
-            }
-            structurePalettes.add(palette);
-        }
+				palette.add(swatch);
+			}
+			structurePalettes.add(palette);
+		}
 
-        // Populate size
-        structureSize.add(new NBTInt(structure.getWidth()));
-        structureSize.add(new NBTInt(structure.getHeight()));
-        structureSize.add(new NBTInt(structure.getLength()));
+		// Populate size
+		structureSize.add(new NBTInt(structure.getWidth()));
+		structureSize.add(new NBTInt(structure.getHeight()));
+		structureSize.add(new NBTInt(structure.getLength()));
 
-        // Create final NBT Compound for structure
-        NBTCompound structureNBT = new NBTCompound();
-        structureNBT.set("blocks", structureBlocks);
-        structureNBT.set("entities", structureEntities);
-        if (structure.getPaletteCount() == 1)
-            structureNBT.set("palette", structurePalettes.get(0));
-        else
-            structureNBT.set("palettes", structurePalettes);
-        structureNBT.set("size", structureSize);
-        structureNBT.set("DataVersion", structureDataVersion);
+		// Create final NBT Compound for structure
+		NBTCompound structureNBT = NBT.Compound(t -> {
+			t.set("blocks", NBT.List(NBTType.TAG_Compound, structureBlocks));
+			t.set("entities", NBT.List(NBTType.TAG_Compound, structureEntities));
+			if (structure.getPaletteCount() == 1) {
+				t.set("palette", NBT.List(NBTType.TAG_Int, structurePalettes.get(0)));
+			} else {
+				t.set("palettes", NBT.List(NBTType.TAG_List,
+						structurePalettes
+								.stream()
+								.map(palette -> NBT.List(NBTType.TAG_Compound, palette))
+								.collect(Collectors.toList())));
+			}
+			t.set("size", NBT.List(NBTType.TAG_Int, structureSize));
+			t.set("DataVersion", structureDataVersion);
+		});
 
-        // Write NBT to file
-        try (NBTWriter writer = new NBTWriter(new File(path), true)) {
-            writer.writeNamed("", structureNBT);
-        }
-    }
+
+		// Write NBT to file
+		try (NBTWriter writer = new NBTWriter(new File(path), CompressedProcesser.GZIP)) {
+			writer.writeNamed("", structureNBT);
+		}
+	}
 }
